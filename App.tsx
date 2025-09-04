1272c89f465e095062b2cee6a06eca34f6bef6a3
Maycon henrique <mayconhs2007@gmail.com>
2025-09-04 09:24:13 -0300

    estilos inline

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world 💩💩!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ backgroundColor: "cornflowerblue", padding: 40 }}>
+        <StatusBar style="auto" />
+        <View>
+          <Text>Dá Hora filmes</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
