315cb995ed007d38849711aeb4c59b5c5f13b883
Maycon henrique <mayconhs2007@gmail.com>
2025-09-25 10:39:57 -0300

    feito

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -6,6 +6,7 @@ import { Filme, ParametrosBusca } from "@/src/types";
 import { useEffect, useState } from "react";
 import { api } from "@/src/services/api";
 import Loading from "@/src/components/Loading";
+import CardFilme from "@/src/components/CardFilme";
 
 export default function Resultados() {
   const { filme } = useLocalSearchParams<ParametrosBusca>();
@@ -58,7 +59,7 @@ export default function Resultados() {
           <FlatList
             data={resultados}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <Text>{item.title}</Text>}
+            renderItem={({ item }) => <CardFilme filme={item} />}
             ListEmptyComponent={<Text>Nenhum filme encontardo !</Text>}
           />
         )}
