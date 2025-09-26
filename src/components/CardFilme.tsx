7edfa511f7fd9bd2a64b539533e89e1e09f5d520
Maycon henrique <mayconhs2007@gmail.com>
2025-09-26 08:27:35 -0300

    feito

--- src/components/CardFilme.tsx
+++ src/components/CardFilme.tsx
@@ -11,11 +11,19 @@ import {
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import { CardFilmeProps } from "../types";
+import { useRouter } from "expo-router";
 
 export default function CardFilme({ filme }: CardFilmeProps) {
   // Extraindo cada prop de dentro do filme
   const { id, title, poster_path } = filme;
-  console.log(id, title, poster_path);
+
+  const router = useRouter();
+
+  const leiaMais = () => {
+    router.push({
+      pathname: "/detalhes/[id]",
+    });
+  };
 
   return (
     <View style={estilos.card}>
@@ -31,7 +39,7 @@ export default function CardFilme({ filme }: CardFilmeProps) {
       <View style={estilos.corpo}>
         <Text style={estilos.titulo}> {title} </Text>
         <View style={estilos.botoes}>
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botao} onPress={leiaMais}>
             <Text style={estilos.textoBotao}>
               <Ionicons name="book" size={12} /> Leia mais
             </Text>
