8a80e0d190627b342c217169104bc2f192c8344d
Maycon henrique <mayconhs2007@gmail.com>
2025-09-30 08:40:49 -0300

    Adiciona funcao apagarTodos

--- app/favoritos.tsx
+++ app/favoritos.tsx
@@ -82,6 +82,13 @@ export default function Favoritos() {
     }
   };
 
+  const apagarTudo = () => {
+    Alert.alert(
+      "❗Apagar todos os favoritos",
+      "Tem certeza que deseja apgar todos os filmes favoritos?"
+    );
+  };
+
   return (
     <>
       <Stack.Screen
